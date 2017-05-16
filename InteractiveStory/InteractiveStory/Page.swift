8777ad02a12c33a9255ab0a51437b4a9b103167c
Iván Martínez <ivanmartinez33@outlook.com>
2017-05-16 14:24:39 -0500

    Helper methods added

--- InteractiveStory/InteractiveStory/Page.swift
+++ InteractiveStory/InteractiveStory/Page.swift
@@ -20,3 +20,27 @@ class Page {
         self.story = story
     }
 }
+
+extension Page {
+    
+    func addChoiceWith(title: String, story: Story) -> Page {
+        let page = Page(story: story)
+        
+        return addChoiceWith(title: title, page: page)
+    }
+    
+    func addChoiceWith(title: String, page: Page) -> Page {
+        
+        switch (firstChoice, secondChoice) {
+        case (.some, .some):
+            return self
+        case (.none, .none), (.none, .some):
+            firstChoice = (title, page)
+        case (.some, .none):
+            secondChoice = (title, page)
+        }
+        
+        return page
+        
+    }
+}
